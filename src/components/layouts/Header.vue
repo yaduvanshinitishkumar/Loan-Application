f481eb1d4a4ff55fef5d259e70a8e01ca8ec0790
Yaduvanshi Nitish Kumar <nitishkumarfreelancer@gmail.com>
2021-08-21 19:15:20 +0530

    Add  avatar and action behaviour to header.

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,12 +1,16 @@
 <template>
   <q-header
-    class="q-py-sm"
+    class="q-py-sm row"
     :class="{
       'transparent text-primary': isLightThemeHeader,
     }"
   >
-    <q-toolbar-title
-      class="text-center text-bold"
+    <!-- Left Branding -->
+    <div class="col"></div>
+
+    <!-- Center Branding -->
+    <div
+      class="col-7 text-center text-bold"
       :class="{ 'text-white': !isLightThemeHeader }"
     >
       <q-avatar>
@@ -15,7 +19,21 @@
         />
       </q-avatar>
       ASPIRE
-    </q-toolbar-title>
+    </div>
+    <!-- Right Action -->
+    <div class="col flex justify-end q-pr-sm">
+      <q-avatar class="pa-none ma-none">
+        <q-btn-dropdown no-icon-animation dropdown-icon="fa fa-user">
+          <q-list>
+            <q-item clickable v-close-popup @click="onItemClick">
+              <q-item-section avatar>
+                <q-btn unelevated> Logout</q-btn>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </q-avatar>
+    </div>
   </q-header>
 </template>
 
